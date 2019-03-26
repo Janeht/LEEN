3dec469d4db2fcd2a35a1f7cac324fa62f9d5e65
Priscila Esther López <2016030010@upsin.edu.mx>
2019-03-26 08:39:10 -0700

    Programación nivel 3

--- app/src/main/java/com/example/root/leen/Nivel3Activity.java
+++ app/src/main/java/com/example/root/leen/Nivel3Activity.java
@@ -17,10 +17,22 @@ public class Nivel3Activity extends AppCompatActivity {
 
     private Button btnRegresar;
     private Button btnReproducir;
-    private ImageView btnPrimeraOP;
-    private ImageView btnSegundaOP;
-    private ImageView btnTerceraOP;
-    private ImageView btnCuartaOP;
+    private ImageButton btnPrimeraOP;
+    private ImageButton btnSegundaOP;
+    private ImageButton btnTerceraOP;
+    private ImageButton btnCuartaOP;
+    private ImageButton btnElegir1;
+    private ImageButton btnElegir2;
+    private ImageButton btnElegir3;
+    private ImageButton btnElegir4;
+    private TextView lbl0;
+    private TextView lbl1;
+    private TextView lbl2;
+    private TextView lbl3;
+    private TextView lbl4;
+    int num = generarNum();
+    int[] direc = {};
+    int imaEl[] = new int[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,108 +45,243 @@ public class Nivel3Activity extends AppCompatActivity {
 
         btnRegresar = (Button) findViewById(R.id.btnRegresar);
         btnReproducir = (Button) findViewById(R.id.btnReproducir);
-        btnPrimeraOP = (ImageView) findViewById(R.id.btnPrimeraOP);
-        btnSegundaOP = (ImageView) findViewById(R.id.btnSegundaOP);
-        btnTerceraOP = (ImageView) findViewById(R.id.btnTerceraOP);
-        btnCuartaOP = (ImageView) findViewById(R.id.btnCuartaOP);
-
-
-        int num = generarNum();
+        btnPrimeraOP = (ImageButton) findViewById(R.id.btnPrimeraOP);
+        btnSegundaOP = (ImageButton) findViewById(R.id.btnSegundaOP);
+        btnTerceraOP = (ImageButton) findViewById(R.id.btnTerceraOP);
+        btnCuartaOP = (ImageButton) findViewById(R.id.btnCuartaOP);
+        btnElegir1 = (ImageButton) findViewById(R.id.btnElegir1);
+        btnElegir2 = (ImageButton) findViewById(R.id.btnElegir2);
+        btnElegir3 = (ImageButton) findViewById(R.id.btnElegir3);
+        btnElegir4 = (ImageButton) findViewById(R.id.btnElegir4);
+        lbl0 = (TextView) findViewById(R.id.lbl0);
+        lbl1 = (TextView) findViewById(R.id.lbl1);
+        lbl2 = (TextView) findViewById(R.id.lbl2);
+        lbl3 = (TextView) findViewById(R.id.lbl3);
+        lbl4 = (TextView) findViewById(R.id.lbl4);
         int num1 = 0;
         int num2 = 0;
         int num3 = 0;
         int num4 = 0;
+
 
         if(num==1){
             num1 = R.drawable.one;
             num2 = R.drawable.two;
             num3 = R.drawable.three;
             num4 = R.drawable.four;
-            int direc[] = {num1, num2, num3, num4};
-
-            int array[] = desordenar(direc);
-            btnPrimeraOP.setImageResource(array[0]);
-            btnSegundaOP.setImageResource(array[1]);
-            btnTerceraOP.setImageResource(array[2]);
-            btnCuartaOP.setImageResource(array[3]);
         }
+
         else if(num==2){
             num1 = R.drawable.two;
             num2 = R.drawable.three;
             num3 = R.drawable.four;
             num4 = R.drawable.five;
-            int direc[] = {num1, num2, num3, num4};
-
-            int array[] = desordenar(direc);
-            btnPrimeraOP.setImageResource(array[0]);
-            btnSegundaOP.setImageResource(array[1]);
-            btnTerceraOP.setImageResource(array[2]);
-            btnCuartaOP.setImageResource(array[3]);
         }
+
         else if(num==3){
             num1 = R.drawable.three;
             num2 = R.drawable.four;
             num3 = R.drawable.five;
             num4 = R.drawable.six;
-            int direc[] = {num1, num2, num3, num4};
-
-            int array[] = desordenar(direc);
-            btnPrimeraOP.setImageResource(array[0]);
-            btnSegundaOP.setImageResource(array[1]);
-            btnTerceraOP.setImageResource(array[2]);
-            btnCuartaOP.setImageResource(array[3]);
         }
+
         else if(num==4){
             num1 = R.drawable.four;
             num2 = R.drawable.five;
             num3 = R.drawable.six;
             num4 = R.drawable.seven;
-            int direc[] = {num1, num2, num3, num4};
-
-            int array[] = desordenar(direc);
-            btnPrimeraOP.setImageResource(array[0]);
-            btnSegundaOP.setImageResource(array[1]);
-            btnTerceraOP.setImageResource(array[2]);
-            btnCuartaOP.setImageResource(array[3]);
         }
+
         else if(num==5){
             num1 = R.drawable.five;
-            num2 = R.drawable.four;
+            num2 = R.drawable.six;
             num3 = R.drawable.seven;
             num4 = R.drawable.eight;
-            int direc[] = {num1, num2, num3, num4};
-
-            int array[] = desordenar(direc);
-            btnPrimeraOP.setImageResource(array[0]);
-            btnSegundaOP.setImageResource(array[1]);
-            btnTerceraOP.setImageResource(array[2]);
-            btnCuartaOP.setImageResource(array[3]);
         }
+
         else if(num==6){
             num1 = R.drawable.six;
             num2 = R.drawable.seven;
             num3 = R.drawable.eight;
             num4 = R.drawable.nine;
-            int direc[] = {num1, num2, num3, num4};
-
-            int array[] = desordenar(direc);
-            btnPrimeraOP.setImageResource(array[0]);
-            btnSegundaOP.setImageResource(array[1]);
-            btnTerceraOP.setImageResource(array[2]);
-            btnCuartaOP.setImageResource(array[3]);
         }
+
         else {
             num1 = R.drawable.seven;
             num2 = R.drawable.eight;
             num3 = R.drawable.nine;
-            num4 = R.drawable.one;
-            int direc[] = {num1, num2, num3, num4};
+            num4 = R.drawable.teen;
+        }
 
-            int array[] = desordenar(direc);
-            btnPrimeraOP.setImageResource(array[0]);
-            btnSegundaOP.setImageResource(array[1]);
-            btnTerceraOP.setImageResource(array[2]);
-            btnCuartaOP.setImageResource(array[3]);
+        direc = desordenar(num1, num2, num3, num4);
+        int[] very = new int[4];
+
+        for(int i=0; i<4; i++){
+            very[i]=direc[i];
+        }
+
+        btnPrimeraOP.setImageResource(direc[0]);
+        btnSegundaOP.setImageResource(direc[1]);
+        btnTerceraOP.setImageResource(direc[2]);
+        btnCuartaOP.setImageResource(direc[3]);
+
+        btnPrimeraOP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir1.getDrawable() == null) {
+                    btnElegir1.setImageResource(direc[0]);
+                    imaEl[0] = direc[0];
+                    lbl1.setText(""+imaEl[0]);
+
+                }
+                else if(btnElegir2.getDrawable() == null){
+                    btnElegir2.setImageResource(direc[0]);
+                    imaEl[0] = direc[0];
+                    lbl1.setText(""+imaEl[0]);
+                }
+
+                else if(btnElegir3.getDrawable() == null){
+                    btnElegir3.setImageResource(direc[0]);
+                    imaEl[0] = direc[0];
+                    lbl1.setText(""+imaEl[0]);
+                }
+                else {
+                    btnElegir4.setImageResource(direc[0]);
+                    imaEl[0] = direc[0];
+                    lbl1.setText(""+imaEl[0]);
+                }
+                btnPrimeraOP.setImageResource(0);
+                btnPrimeraOP.setEnabled(false);
+            }
+        });
+
+        btnSegundaOP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir1.getDrawable() == null) {
+                    btnElegir1.setImageResource(direc[1]);
+                    imaEl[1] = direc[1];
+                    lbl2.setText(""+imaEl[1]);
+                }
+                else if(btnElegir2.getDrawable() == null){
+                    btnElegir2.setImageResource(direc[1]);
+                    imaEl[1] = direc[1];
+                    lbl2.setText(""+imaEl[1]);
+                }
+
+                else if(btnElegir3.getDrawable() == null){
+                    btnElegir3.setImageResource(direc[1]);
+                    imaEl[1] = direc[1];
+                    lbl2.setText(""+imaEl[1]);
+                }
+                else {
+                    btnElegir4.setImageResource(direc[1]);
+                    imaEl[1] = direc[1];
+                    lbl2.setText(""+imaEl[1]);
+                }
+                btnSegundaOP.setImageResource(0);
+                btnSegundaOP.setEnabled(false);
+            }
+        });
+
+        btnTerceraOP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir1.getDrawable() == null) {
+                    btnElegir1.setImageResource(direc[2]);
+                    imaEl[2] = direc[2];
+                    lbl3.setText(""+imaEl[2]);
+                }
+                else if(btnElegir2.getDrawable() == null){
+                    btnElegir2.setImageResource(direc[2]);
+                    imaEl[2] = direc[2];
+                    lbl3.setText(""+imaEl[2]);
+                }
+
+                else if(btnElegir3.getDrawable() == null){
+                    btnElegir3.setImageResource(direc[2]);
+                    imaEl[2] = direc[2];
+                    lbl3.setText(""+imaEl[2]);
+                }
+                else {
+                    btnElegir4.setImageResource(direc[2]);
+                    imaEl[2] = direc[2];
+                    lbl3.setText(""+imaEl[2]);
+                }
+                btnTerceraOP.setImageResource(0);
+                btnTerceraOP.setEnabled(false);
+            }
+        });
+
+        btnCuartaOP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir1.getDrawable() == null) {
+                    btnElegir1.setImageResource(direc[3]);
+                    imaEl[3] = direc[3];
+                    lbl4.setText(""+imaEl[3]);
+                }
+                else if(btnElegir2.getDrawable() == null){
+                    btnElegir2.setImageResource(direc[3]);
+                    imaEl[3] = direc[3];
+                    lbl4.setText(""+imaEl[3]);
+                }
+
+                else if(btnElegir3.getDrawable() == null){
+                    btnElegir3.setImageResource(direc[3]);
+                    imaEl[3] = direc[3];
+                    lbl4.setText(""+imaEl[3]);
+                }
+                else {
+                    btnElegir4.setImageResource(direc[3]);
+                    imaEl[3] = direc[3];
+                    lbl4.setText(""+imaEl[3]);
+                }
+                btnCuartaOP.setImageResource(0);
+                btnCuartaOP.setEnabled(false);
+            }
+        });
+
+        btnElegir1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir1.getDrawable() != null){
+                    ActivarbtnOp();
+                }
+            }
+        });
+
+        btnElegir2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir2.getDrawable() != null){
+                    ActivarbtnOp();
+                }
+            }
+        });
+
+        btnElegir3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir3.getDrawable() != null){
+                    ActivarbtnOp();
+                }
+            }
+        });
+
+        btnElegir4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnElegir4.getDrawable() != null){
+                    ActivarbtnOp();
+                }
+            }
+        });
+
+
+        if(btnPrimeraOP.isEnabled() == false && btnSegundaOP.isEnabled() == false &&
+                btnTerceraOP.isEnabled() == false && btnCuartaOP.isEnabled() == false){
+            lbl0.setText("El array está lleno");
         }
 
         btnRegresar.setOnClickListener(new View.OnClickListener() {
@@ -154,14 +301,33 @@ public class Nivel3Activity extends AppCompatActivity {
         return r.nextInt(8)+1;
     }
 
-    public int[] desordenar(int[] array){
+    public int[] desordenar(int num1, int num2, int num3, int num4){
+        int array[]= {num1, num2, num3, num4};
         int lon = array.length-1;
+
         for(int i = lon; i>1; i--){
             int x = (int) Math.floor(i*Math.random());
             int y = array[i];
             array[i] = array[x];
             array[x] = y;
         }
-        return (array);
+        return array;
+    }
+
+    public void ActivarbtnOp(){
+        btnElegir1.setImageResource(0);
+        btnElegir2.setImageResource(0);
+        btnElegir3.setImageResource(0);
+        btnElegir4.setImageResource(0);
+
+        btnPrimeraOP.setEnabled(true);
+        btnSegundaOP.setEnabled(true);
+        btnTerceraOP.setEnabled(true);
+        btnCuartaOP.setEnabled(true);
+
+        btnPrimeraOP.setImageResource(direc[0]);
+        btnSegundaOP.setImageResource(direc[1]);
+        btnTerceraOP.setImageResource(direc[2]);
+        btnCuartaOP.setImageResource(direc[3]);
     }
 }
